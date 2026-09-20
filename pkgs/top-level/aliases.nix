f54d8870c3d45dc0b46f7f1e9e306dea6ff74dca
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 15:41:55 -0300

    obitools3: drop

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1920,6 +1920,7 @@ mapAliases {
   oathToolkit = throw "'oathToolkit' has been renamed to/replaced by 'oath-toolkit'"; # Converted to throw 2025-10-27
   obb = throw "obb has been removed because it has been marked as broken since 2023."; # Added 2025-10-11
   obexfs = throw "'obexfs'  has been removed as it was unmaintained upstream"; # Added 2026-05-31
+  obitools3 = throw "'obitools3' has been removed as it was unmaintained"; # Added 2026-09-20
   obliv-c = throw "obliv-c has been removed from Nixpkgs, as it has been unmaintained upstream for 4 years and does not build with supported GCC versions"; # Added 2025-08-18
   ocis = throw "'ocis' has been removed because the package was lagging three major versions behind upstream"; # added 2026-08-02
   oclgrind = throw "oclgrind has been removed, as it does not build with supported LLVM versions"; # Added 2025-08-10
