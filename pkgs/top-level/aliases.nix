7e94369c1a5cae7eb95626545540bac12f5af506
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 14:09:20 -0300

    em: drop

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -776,6 +776,7 @@ mapAliases {
   elixir_1_19 = warnAlias "'elixir_1_19' is deprecated in favor of using the beamPackages sets. Use 'beamPackages.elixir_1_19' instead." beamPackages.elixir_1_19; # added 2026-06-15
   elixir_ls = throw "'elixir_ls' has been renamed to/replaced by 'elixir-ls'"; # Converted to throw 2025-10-27
   elm-github-install = throw "'elm-github-install' has been removed as it is abandoned upstream and only supports Elm 0.18.0"; # Added 2025-08-25
+  em = throw "'em' has been removed as it was unmaintained"; # Added 2026-09-20
   emacs30 = throw "'emacs30' has been superseded by 'emacs' on august 2026"; # Added 2026-08-25
   emacs30-gtk3 = throw "'emacs30-gtk3' has been superseded by 'emacs-gtk3' on august 2026"; # Added 2026-08-25
   emacs30-nox = throw "'emacs30-nox' has been superseded by 'emacs-nox' on august 2026"; # Added 2026-08-25
