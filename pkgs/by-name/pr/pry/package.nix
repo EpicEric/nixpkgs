ff090a3d5c38377e73c790ddba9c36f1fb9f66c9
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 16:17:54 -0300

    pry: remove homepage

--- pkgs/by-name/pr/pry/package.nix
+++ pkgs/by-name/pr/pry/package.nix
@@ -13,7 +13,7 @@ bundlerApp {
 
   meta = {
     description = "Ruby runtime developer console and IRB alternative";
-    homepage = "https://pryrepl.org";
+    homepage = [ ];
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.tckmn ];
     platforms = lib.platforms.unix;
