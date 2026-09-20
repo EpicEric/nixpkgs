490185c521135a71ac1e5d2724324bb54d17a2e3
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 15:14:46 -0300

    lerpn: drop

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1340,6 +1340,7 @@ mapAliases {
   ledger_agent = throw "ledger-agent has been removed because upstream dropped Ledger support"; # Added 2026-03-11
   lenpaste = throw "'lenpaste' has been removed as it was unmaintained"; # Added 2026-09-20
   lepton = throw "'lepton' has been removed as it is archived upstream and has known vulnerabilities (CVE-2022-4104). Consider using 'lepton-jpeg-util' instead"; # Added 2026-06-29
+  lerpn = throw "'lerpn' has been removed as it was unmaintained"; # Added 2026-09-20
   lesstif = throw "'lesstif' has been removed due to its being broken and unmaintained upstream. Consider using 'motif' instead."; # Added 2025-06-09
   lexical = throw "'lexical' has been removed because it was deprecated and archived upstream. Consider using 'beamPackages.expert' instead"; # Added 2026-02-24
   lfe = warnAlias "'lfe' is deprecated in favor of using the beamPackages sets. Use 'beam27Packages.lfe' instead." beam27Packages.lfe; # added 2026-06-15
