81f295a67fbcba411ff36afe005d848fcc61eef8
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-20 16:53:12 -0300

    scmccid: remove homepage

--- pkgs/by-name/sc/scmccid/package.nix
+++ pkgs/by-name/sc/scmccid/package.nix
@@ -47,7 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   meta = {
-    homepage = "http://support.identiv.com/products";
+    homepage = [ ];
     description = "PCSC drivers for linux, for the SCM SCR3310 v2.0 card and others";
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     license = lib.licenses.unfree;
